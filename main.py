011232346a93d056a10544f9d8283d94e6e116aa
Pravesh Bawangade <pptechno52@gmail.com>
2020-05-22 15:57:21 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import torch
+from torchvision import transforms
+from torch.autograd import Variable
+import PIL.Image as Image
+import cv2
+import numpy as np
+import model as m
+import os
+
+
+def predict_image(image, model):
+    noise_factor = 0.3
+    image = Image.fromarray(image.astype('uint8'), 'RGB')
+
+    test_transforms = transforms.Compose(
+        [transforms.Resize(128),
+         transforms.ToTensor(),
+         transforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3)])
+
+    image_tensor = test_transforms(image).float()
+    image_tensor = image_tensor.unsqueeze_(0)
+    image_tensor = Variable(image_tensor)
+
+    noisy_imgs = image_tensor + noise_factor * torch.randn(*image_tensor.shape)
+    noisy_imgs = np.clip(noisy_imgs, 0., 1.)
+    noisy_imgs = noisy_imgs.to('cpu')
+
+    output = model(noisy_imgs)
+    output = output.detach().numpy()
+    return noisy_imgs, output
+
+
+def main():
+    model = m.ConvDenoiser()
+    model.eval()
+    model.load_state_dict(torch.load('trained_models/train-epoch9.pth', map_location=torch.device('cpu')))
+
+    root = 'images/'
+    img_list = os.listdir(root)
+
+    for i in range(len(img_list)):
+        print(root + img_list[i])
+        if img_list[i] != ".DS_Store":
+            image = cv2.imread(root + img_list[i])
+            image = cv2.resize(image,(130,130))
+
+            noisy_imgs, output = predict_image(image, model)
+
+            noisy_imgs = noisy_imgs[0].numpy().transpose(1, 2, 0)
+            output = output[0].transpose(1, 2, 0)
+            vis = np.concatenate((noisy_imgs, output), axis=1)
+
+            frame_normed = 255 * (vis - vis.min()) / (vis.max() - vis.min())
+            frame_normed = np.array(frame_normed, np.int)
+
+            cv2.imwrite("output_images/output-" + img_list[i], frame_normed)
+            cv2.imshow("output", vis)
+            cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
---
+++ model.py
@@ -0,0 +1,61 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# define the NN architecture
+class ConvDenoiser(nn.Module):
+    def __init__(self):
+        super(ConvDenoiser, self).__init__()
+
+        # encoder layers #
+
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+
+        self.conv2 = nn.Conv2d(32, 16, 3, padding=1)
+
+        self.conv3 = nn.Conv2d(16, 8, 3, padding=1)
+
+        # self.conv4 = nn.Conv2d(16, 8, 3, padding=1)
+        # pooling layer to reduce x-y dims by two; kernel and stride of 2
+        self.pool = nn.MaxPool2d(2, 2)
+
+        ## decoder layers ##
+        # transpose layer, a kernel of 2 and a stride of 2 will increase the spatial dims by 2
+        self.t_conv1 = nn.ConvTranspose2d(8, 8, 2, stride=2)  # kernel_size=3 to get to a 7x7 image output
+        # two more transpose layers with a kernel of 2
+        self.t_conv2 = nn.ConvTranspose2d(8, 16, 2, stride=2)
+        self.t_conv3 = nn.ConvTranspose2d(16, 32, 2, stride=2)
+
+        # one, final, normal conv layer to decrease the depth
+        self.conv_out = nn.Conv2d(32, 3, 3, padding=1)
+
+    def forward(self, x):
+        ## encode ##
+        # add hidden layers with relu activation function
+        # and maxpooling after
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+
+        # add second hidden layer
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+
+        # add third hidden layer
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
+
+        # add fourth hidden layer
+        # x = F.relu(self.conv4(x))
+        # x = self.pool(x) # compressed representation
+
+        ## decode ##
+        # add transpose conv layers, with relu activation function
+        x = F.relu(self.t_conv1(x))
+        x = F.relu(self.t_conv2(x))
+        x = F.relu(self.t_conv3(x))
+        # x = F.relu(self.t_conv4(x))
+        # transpose again, output should have a sigmoid applied
+        x = F.relu(self.conv_out(x))
+
+        return x
+
